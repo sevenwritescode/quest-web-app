52ee3e116378efa999b9cda45fced1fd06e33b1c
sevenwritescode <github.portal036@slmails.com>
2025-08-29 12:43:34 -0700

    re init the server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,10 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <> 
+      <div> 
+
+      </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
