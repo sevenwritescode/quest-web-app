518bff6389a17f559a0175057b5ee4d6e7806656
sevenwritescode <github.portal036@slmails.com>
2025-08-29 13:07:42 -0700

    updated serve

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,22 @@
 module.exports = {
-  apps: [
-    {
-      name: 'api',
-      cwd: './server',
-      script: 'node',
-      args: './dist/index.js',
-      instances: 'max',           // scale across CPUs (optional)
-      exec_mode: 'cluster',       // cluster mode for Node API
-      env_production: {
-        NODE_ENV: 'production'
-        // other production env vars
-      },
-      max_memory_restart: '300M',
-      watch: false,
-      merge_logs: true,
-      error_file: './logs/api-err.log',
-      out_file: './logs/api-out.log'
-    }
-  ]
-};
+       apps: [
+         {
+           name: 'api',
+           cwd: './server',
+           script: './dist/index.js',
+           env: {
+             NODE_ENV: 'production',
+             // your other env vars or let PM2 inherit from process.env/.env
+           }
+         },
+         {
+           name: 'client',
+           cwd: './client',
+           script: 'npm',
+           args: 'run serve',
+           env: {
+             NODE_ENV: 'production'
+           }
+         }
+       ]
+     }
