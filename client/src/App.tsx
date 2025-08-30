299102ea9c99fbe566a49fbbb7ceaef6b0d3da71
sevenwritescode <github.portal036@slmails.com>
2025-08-29 18:20:51 -0700

    changed the buttons a bit, doing client side validation for code being only length-4 and alpha

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,52 +1,36 @@
 import { Component } from 'react';
+import Landing from './Landing';
 
-// Define props interface
 interface AppProps {
-  // Add your props here
 }
 
-// Define state interface
 interface AppState {
-  // Add your state properties here
+  page: Page
 }
+
+type Page = { kind: "landing", code: string } | { kind: "lobby" } | { kind: "game" }
 
 class App extends Component<AppProps, AppState> {
   constructor(props: AppProps) {
     super(props);
     this.state = {
-      // Initialize your state here
+      page: { kind: "landing", code: "" }
     };
   }
 
-  render() {
-    return (
-    <>
-      <div className="flex justify-center text-center align-middle font-[Chomsky] text-4xl  p-10">
-        <h1 className=""> The <span className="text-red-700">Quest</span> Web App Companion </h1>
-      </div>
+  render = () => {
+    if (this.state.page.kind === "landing") {
+      return <Landing code={this.state.page.code} onCodeChange={this.doUpdateCode}></Landing>
+    }
+  }
 
-      <div className="flex justify-center flex-col space-x-6 items-center font-[Consolas]">
-        <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 m-1">
-          Host
-        </button>
-        <div className="flex items-center space-x-2 m-2">
-          
-          <button className="px-3 py-2 bg-green-600 text-white rounded hover:bg-green-700 m-1" 
-            onClick={() => console.log("click!")}>  
-            <input
-              type="text"
-              size={4}
-              onClick={(e) => e.stopPropagation()}
-              className="rounded bg-green-800 px-2 py-1 w-[6ch] mr-2 text-center placeholder-gray-400"
-              placeholder="----"
-            />
-            Join
-          </button>
-        </div>
-      </div>
-    
-    </>
-    );
+  doUpdateCode = (code: string) => {
+    if (this.state.page.kind !== "landing") {
+      throw new Error("App is in an invalid state: attempt to update code while not on landing page");
+    }
+    this.setState({
+      page: { kind: "landing", code }
+    });
   }
 }
 
---
+++ client/src/Landing.tsx
@@ -0,0 +1,59 @@
+import { Component } from "react";
+
+interface LandingProps {
+  code: string,
+  onCodeChange: (code: string) => void
+}
+
+class Landing extends Component<LandingProps> {
+  constructor(props: LandingProps) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+        <div className="flex justify-center text-center align-middle font-[Chomsky] text-4xl  p-10">
+          <h1 className=""> The <span className="text-[rgb(212,51,51)]">Quest</span> Web App Companion </h1>
+        </div>
+
+        <div className="flex justify-center flex-col space-x-6 items-center font-[Consolas]">
+          <button className="h-12 w-28 bg-[rgb(230,97,2)] text-white rounded hover:bg-[rgb(184,77,0)] m-1 flex items-center justify-center">
+            Host
+          </button>
+          <div className="flex items-center space-x-2 m-2">
+
+            <button className="h-12 w-28 bg-green-600 text-white rounded hover:bg-green-700 m-1 flex items-center justify-center"
+              onClick={() => console.log("click!")}>
+              <input
+                type="text"
+                size={4}
+                onClick={(e) => e.stopPropagation()}
+                value={this.props.code}
+                onChange={this.validateCodeInput}
+                className="rounded bg-green-800 h-8 px-2 mr-2 text-left placeholder-gray-400"
+                placeholder="----"
+              />
+              Join
+            </button>
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  validateCodeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let input = event.target.value;
+    // drop all non alpha characters
+    input = input.replace(/[^A-Za-z]/g, '');
+    console.log(input);
+    if (input.length > 4) {
+      // input is too long, drop the end
+      input = input.slice(0, 4);
+    }
+    this.props.onCodeChange(input.toUpperCase());
+  }
+}
+
+
+export default Landing;
